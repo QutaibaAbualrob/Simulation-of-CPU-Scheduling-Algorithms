f29d7c2150e816d1e5f08512bc3945487dbf94fe
qutaibaaubalrob <qotaibawaleed@gmail.com>
2025-01-16 19:15:04 +0200

    Changed the datastructure to class

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -8,7 +8,7 @@ using System.Threading;
 class filler
 {
 
-    public static void fillJobsWithArrivalTime(Dictionary<int, long[]> jobs, int processCount)
+    public static void fillJobsWithArrivalTime(process[] processArray, int processCount)
     {
 
 
@@ -18,21 +18,24 @@ class filler
         time.Start();
         for (int i = 0; i < processCount; i++)
         {
-            jobs[i][0] = time.ElapsedMilliseconds;
-            Thread.Sleep(339);
+           
+
+            processArray[i].ArrivalTime = time.ElapsedMilliseconds;
+            var rand = new Random();
+            Thread.Sleep(rand.Next(1,500));
         }
         time.Stop();
     }
 
-    public static void fillJobsWithBurstTime(Dictionary<int, long[]> jobs, int processCount)
+    public static void fillJobsWithBurstTime(process[] processArray, int processCount)
     {
         for (int i = 0; i < processCount; i++)
         {
-            jobs[i][1] = jobs[i][0] * 10;
+            processArray[i].BurstTime = processArray[i].ArrivalTime * 10;
         }
     }
 
-    public static void fillJobsWithPriority(Dictionary<int, long[]> jobs, int processCount)
+    public static void fillJobsWithPriority(process[] processArray, int processCount)
     {
         Random rand = new Random();
         List<int> numbers = Enumerable.Range(1, processCount).ToList();
@@ -41,7 +44,7 @@ class filler
         for (int i = 0; i < processCount; i++)
         {
 
-            jobs[i][2] = numbers[i];
+            processArray[i].ProcessPriority = numbers[i];
 
         }
     }
@@ -51,17 +54,16 @@ class filler
 
 class print
 {
-   public static void printJobs(Dictionary<int, long[]> jobs)
-    {
+   public static void printJobs(process[] processArray)   {
         int count = 1;
-        foreach (var job in jobs)
+        foreach (var process in processArray)
         {
-            var value = job.Value;
+            
 
             Console.WriteLine($"Process id {count}");
-            Console.WriteLine($"This is process arrival time {value[0]}ms");
-            Console.WriteLine($"This is process burst time {value[1]}ms");
-            Console.WriteLine($"This is process priority {value[2]}");
+            Console.WriteLine($"This is process Arrival time {process.ArrivalTime}ms");
+            Console.WriteLine($"This is process Burst time {process.BurstTime}ms");
+            Console.WriteLine($"This is process Priority {process.ProcessPriority}");
             Console.WriteLine($"______________________________");
 
 
@@ -74,7 +76,78 @@ class print
 
 }
 
+class process
+{
 
+    /*
+            Jobs dictionary where the key is the process ID which is a number from 1 to 5 (5 processes):
+            jobs[i][0-2]:
+
+            jobs[i][0] is arrival time
+            jobs[i][1] is CPU Burst time
+            jobs[i][2] is Process Priority
+
+         */
+    long arrivalTime;
+    long burstTime;
+    long processPriority;
+
+    public long ArrivalTime
+    {
+        get
+        {
+            return arrivalTime;
+        }
+
+        set
+        {
+            this.arrivalTime = value;
+        }
+    }
+
+    public long BurstTime
+    {
+        get
+        {
+            return burstTime;
+        }
+        
+        set
+        {
+            this.burstTime = value;
+        }
+    }
+
+    public long ProcessPriority
+    {
+        get
+        {
+            return processPriority;
+        }
+
+        set
+        {
+            this.processPriority = value;
+        }
+    }
+
+
+
+
+    public process(int arrivalTime, int burstTime, int processPriority)
+    {
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.processPriority = processPriority;
+    }
+
+    public process()
+    {
+        arrivalTime = 0;
+        burstTime = 0;
+        processPriority = 0;
+    }
+}
 
 
 class FCFS
@@ -99,13 +172,13 @@ class Program
     {
         
 
-        var jobs = new Dictionary<int, long[]>();
+        
         int n = 5;
-        for (int i = 0; i< n; i++) 
+        process[] processArray = new process[n];
+        for (int i = 0; i < n; i++)
         {
-            jobs[i]= new long[n];
+            processArray[i] = new process();
         }
-
 
         /*
             Jobs dictionary where the key is the process ID which is a number from 1 to 5 (5 processes):
@@ -118,15 +191,14 @@ class Program
          */
 
 
-
-        filler.fillJobsWithArrivalTime(jobs, n);
-        filler.fillJobsWithBurstTime(jobs, n);
-        filler.fillJobsWithPriority(jobs, n);
-
+        filler.fillJobsWithArrivalTime(processArray, n);
+        filler.fillJobsWithBurstTime(processArray, n);
+        filler.fillJobsWithPriority(processArray, n);
 
 
 
-        print.printJobs(jobs);
+
+        print.printJobs(processArray);
         
     }
 }
